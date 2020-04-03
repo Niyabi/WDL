2fff5e3c6aff49e55a89d7d9abce2c94d34eeddf
Niyabi <kufel92@gmail.com>
2020-04-03 11:33:24 +0200

    Start and Stop scripts

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+service apache2 start
+service mysql start
+service php7.2-fpm start
+service php7.2-fpm restart
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+service php7.2-fpm stop
+service mysql stop
+service apache2 stop
